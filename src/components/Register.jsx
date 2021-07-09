31ff52f0e5fdba34a909d87e6650903a4025558a
SeanKurtz97 <seankurtz75@gmail.com>
2021-07-09 00:20:06 -0500

    Added username and password confirm to sign in w/validate

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,78 @@
 import React from "react";
-import { Box, Heading, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Link,
+  Text,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+} from "@chakra-ui/react";
+
 import { Link as RouterLink } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
+import { validateEmail, validatePassword, validateUsername } from "../validate";
 
+const RegisterForm = () => {
+  return (
+    <Formik
+      initialValues={{ email: "", username: "", password: "" }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Stack spacing="6">
+            <Field name="email" validate={validateEmail}>
+              {({ field, form }) => (
+                <FormControl
+                  isInvalid={form.errors.email && form.touched.email}
+                >
+                  <FormLabel htmlFor="email">Email address</FormLabel>
+                  <Input {...field} id="email" />
+                  <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="username" validate={validateUsername}>
+              {({ field, form }) => (
+                <FormControl
+                  isInvalid={form.errors.username && form.touched.username}
+                >
+                  <FormLabel htmlFor="username">Username</FormLabel>
+                  <Input {...field} id="username" />
+                  <FormErrorMessage>{form.errors.username}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="password" validate={validatePassword}>
+              {({ field, form }) => (
+                <FormControl
+                  isInvalid={form.errors.password && form.touched.password}
+                >
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Input {...field} id="password" type="password" />
+                  <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+
+            <Button type="submit" colorScheme="blue" isLoading={isSubmitting}>
+              Register
+            </Button>
+          </Stack>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 const RegisterCard = ({ children }) => {
   return (
     <Box bg="white" py="8" px="4" shadow="base" rounded="lg">
@@ -37,7 +107,7 @@ const Register = () => {
     <Box maxW="md" mx="auto">
       <RegisterHeading />
       <RegisterCard>
-        <Heading>Register</Heading>
+        <RegisterForm />
       </RegisterCard>
     </Box>
   );
